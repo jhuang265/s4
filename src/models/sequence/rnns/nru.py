2b1f9f1b745754dfe24648f8382ab3909dedc65c
Jerry <jerryhuang265@hotmail.com>
2024-02-27 16:03:02 -0500

    Update NRU to use different number of heads

--- src/models/sequence/rnns/nru.py
+++ src/models/sequence/rnns/nru.py
@@ -362,6 +362,7 @@ class NRU(nn.Module):
         v_plus_nonlin='sigmoid',
         alpha_minus_nonlin='tanh', 
         v_minus_nonlin='tanh',
+        num_heads=4, rank=1, norm_p=5,
         dropout=0.0,
         tie_dropout=False,
         transposed=True,
@@ -374,12 +375,19 @@ class NRU(nn.Module):
         self.d_state = d_hidden
         self.d_memory = d_memory
         
+        self.num_heads = num_heads
+        self.norm_p = norm_p
+        
         self.alpha_plus_nonlin = nonlin(alpha_plus_nonlin)
         self.v_plus_nonlin = nonlin(v_plus_nonlin)
         self.alpha_minus_nonlin = nonlin(alpha_minus_nonlin)
-        self.v_minus_nonlin = nonlin(v_minus_nonlin)
+        self.v_minus_nonlin = nonlin(v_minus_nonlin)self.rank = rank
         
-        self.alpha_v = nn.Linear(self.d_input, 4 * self.d_memory, bias=alpha_v_bias)
+        sqrt_dim = int(math.sqrt(num_heads * m_features))
+        self.sqrt_dim = sqrt_dim
+        self.alpha_v = nn.Linear(in_features, 2 * num_heads + 4 * sqrt_dim * rank, bias=alpha_v_bias)
+        
+        # self.alpha_v = nn.Linear(self.d_input, 4 * self.d_memory, bias=alpha_v_bias)
         self.B = nn.Linear(self.d_input, self.d_state, bias=False)
         
         layer_cfg = layer_args.copy()
@@ -404,10 +412,30 @@ class NRU(nn.Module):
         x = input_sequence
         b, l, inp_dim = x.shape[0], x.shape[1], x.shape[2]
         # input for M
-        alpha_v = self.alpha_v(x[:, :-1])
+        alpha_v = self.alpha_v(x)
+        # print(alpha_v.shape)
+        alpha, beta = alpha_v[..., :self.num_heads], alpha_v[..., self.num_heads:self.num_heads * 2]
+        # print(alpha_v[..., 2 * self.num_heads:].shape)
+        v = alpha_v[..., 2 * self.num_heads:].view(b, l, 4, self.rank, self.sqrt_dim)
+        # print(v.shape)
+        v_alpha1, v_alpha2, v_beta1, v_beta2 = \
+        v[:, :, 0].contiguous(), \
+        v[:, :, 1].contiguous(), \
+        v[:, :, 2].contiguous(), \
+        v[:, :, 3].contiguous()
+        v_alpha = torch.einsum('btkd,btkp->btdp', v_alpha1, v_alpha2).view(b,l, self.num_heads, self.m_features)
+        # print(v_alpha.shape)
+        v_alpha = F.normalize(v_alpha, p=self.norm_p, dim=3, eps=1e-12)
+        # v_alpha = v_alpha / v_alpha.max(3, keepdim=True)[0]
+        v_beta = torch.einsum('btkd,btkp->btdp', v_beta1, v_beta2).view(b,l, self.num_heads, self.m_features)
+        # print(v_beta.shape)
+        v_beta = F.normalize(v_beta, p=self.norm_p, dim=3, eps=1e-12)
+        # v_beta = v_beta / v_beta.max(3, keepdim=True)[0]
+        alpha = torch.einsum('btk,btkd->btd', alpha, v_alpha)
+        beta = torch.einsum('btk,btkd->btd', beta, v_beta)
+        # print(alpha.shape, beta.shape)
+        ms_inp = alpha - beta
         m_init = torch.zeros((b, 1, d), dtype=x.dtype, device=x.device)
-        ms_inp = (self.alpha_plus_nonlin(alpha_v[..., :d]) * self.v_plus_nonlin(alpha_v[..., d:2*d])
-                - self.alpha_minus_nonlin(alpha_v[..., 2*d:3*d]) * self.v_minus_nonlin(alpha_v[..., 3*d:4*d]))
         
         # TODO: if you need a more granular control over biases,
         # uncomment these
@@ -421,7 +449,7 @@ class NRU(nn.Module):
         ms = associative_scan(bin_op_cumsum, ms_inp, axis=1)
         ms = torch.cat([m_init, ms], dim=1)
         # ms = ms.float()
-        ssm_inp = torch.cat([x, ms], axis=2) # size is [batch, length, in_size + m_size]
+        ssm_inp = torch.cat([x.to(ms.dtype), ms], axis=2) # size is [batch, length, in_size + m_size]
         return self.ssm.forward_parallel(ssm_inp)
     
     def forward(self, x, state=None, **kwargs):
@@ -437,17 +465,58 @@ class NRU(nn.Module):
         x = input_sequence
         b, l, inp_dim = x.shape[0], x.shape[1], x.shape[2]
         # input for M
-        alpha_v = self.alpha_v(x[:, :-1])
-        ms_inp = (self.alpha_plus_nonlin(alpha_v[..., :d]) * self.v_plus_nonlin(alpha_v[..., d:2*d])
-                - self.alpha_minus_nonlin(alpha_v[..., 2*d:3*d]) * self.v_minus_nonlin(alpha_v[..., 3*d:4*d]))
-        ms_inp = torch.cat([m_state.unsqueeze(1), ms_inp], dim=1)
+
+        # palpha_v = self.alpha_v(x)
+        # # print(alpha_v.shape)
+        # palpha, pbeta = palpha_v[..., :self.num_heads], palpha_v[..., self.num_heads:self.num_heads * 2]
+        # # print(alpha_v[..., 2 * self.num_heads:].shape)
+        # pv = palpha_v[..., 2 * self.num_heads:].view(b, l, 4, self.rank, self.sqrt_dim)
+        # # print(v.shape)
+        # pv_alpha1, pv_alpha2, pv_beta1, pv_beta2 = \
+        #   pv[:, :, 0].contiguous(), \
+        #   pv[:, :, 1].contiguous(), \
+        #   pv[:, :, 2].contiguous(), \
+        #   pv[:, :, 3].contiguous()
+        # pv_alpha = torch.einsum('btkd,btkp->btdp', pv_alpha1, pv_alpha2).view(b,l, self.num_heads, self.m_features)
+        # # print(v_alpha.shape)
+        # pv_alpha = F.normalize(pv_alpha, p=self.norm_p, dim=3, eps=1e-12)
+        # pv_beta = torch.einsum('btkd,btkp->btdp', pv_beta1, pv_beta2).view(b,l, self.num_heads, self.m_features)
+        # # print(v_beta.shape)
+        # v_beta = F.normalize(v_beta, p=self.norm_p, dim=3, eps=1e-12)
+        # alpha = torch.einsum('btk,btkd->btd', alpha, v_alpha)
+        # beta = torch.einsum('btk,btkd->btd', beta, v_beta)
+        # # print(alpha.shape, beta.shape)
+        # ms_inp = (alpha - beta)
+
+        # ms_inp = torch.cat([m_state.unsqueeze(1), ms_inp], dim=1)
         # TODO: if you need a more granular control over biases,
         # uncomment these
         # ms_inp = self.alpha_plus(x) * self.v_plus(x) - self.alpha_minus(x) * self.v_minus(x)
-        ms = torch.zeros(ms_inp.shape, dtype=ms_inp.dtype, device=ms_inp.device)
-        for j in range(1, ms_inp.shape[1]):
-            ms[:, j] = ms[:, j - 1] + ms_inp[:, j]
-            
+        ms = torch.zeros((b, l, d), dtype=x.dtype, device=x.device)
+        ms[:, 0] = m_state
+        for j in range(1, x.shape[1]):
+            alpha_v = self.alpha_v(x[:, j-1]) # [b, in_dim]
+            alpha, beta = alpha_v[..., :self.num_heads], alpha_v[..., self.num_heads:self.num_heads * 2]
+            # print(alpha_v[..., 2 * self.num_heads:].shape)
+            v = alpha_v[..., 2 * self.num_heads:].view(b, 4, self.rank, self.sqrt_dim)
+            # print(v.shape)
+            v_alpha1, v_alpha2, v_beta1, v_beta2 = \
+                v[:, 0].contiguous(), \
+                v[:, 1].contiguous(), \
+                v[:, 2].contiguous(), \
+                v[:, 3].contiguous()
+            v_alpha = torch.einsum('bkd,bkp->bdp', v_alpha1, v_alpha2).view(b, self.num_heads, self.m_features)
+            # print(v_alpha.shape)
+            v_alpha = F.normalize(v_alpha, p=self.norm_p, dim=2, eps=1e-12)
+            # v_alpha = v_alpha / v_alpha.max(2, keepdim=True)[0]
+            v_beta = torch.einsum('bkd,bkp->bdp', v_beta1, v_beta2).view(b, self.num_heads, self.m_features)
+            # print(v_beta.shape)
+            v_beta = F.normalize(v_beta, p=self.norm_p, dim=2, eps=1e-12)
+            # v_beta = v_beta / v_beta.max(2, keepdim=True)[0]
+            alpha = torch.einsum('bk,bkd->bd', alpha, v_alpha)
+            beta = torch.einsum('bk,bkd->bd', beta, v_beta)
+            # print(alpha.shape, beta.shape)
+            ms[:, j] = ms[:, j - 1] + alpha - beta
         ssm_inp = torch.cat([x, ms], axis=2) # size is [batch, length, in_size + m_size]
         return *self.ssm.forward_sequential(ssm_inp, state), ms[:, -1]
     
